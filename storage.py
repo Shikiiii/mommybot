143e2aa09eaee9e1511145243f70cc83ea91ea61
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-03-18 15:54:10 +0200

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,1 +1,9 @@
+Learn more or give us feedback
+from discord.ext.commands import Bot
+from discord.ext import commands
+import discord
+import http.client
+import requests
+import json
 
+bot = commands.Bot(command_prefix="=", case_insensitive=True)
