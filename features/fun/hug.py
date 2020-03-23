642bd296d33632e5649c80abc3e86545c20dcb61
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-03-23 12:29:37 +0200

    Update hug.py

--- features/fun/hug.py
+++ features/fun/hug.py
@@ -5,7 +5,7 @@ import datetime
 import asyncio
 import random
 from datetime import datetime
-from cv import *
+from storage import *
 
 hug_gifs = [
     "https://cdn.discordapp.com/attachments/671404646459244616/672068771254632478/a.gif",
