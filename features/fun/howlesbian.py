808a4f802c729f46de2c2903119931ac18c4df72
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-03-30 14:35:58 +0300

    Update howlesbian.py

--- features/fun/howlesbian.py
+++ features/fun/howlesbian.py
@@ -17,11 +17,11 @@ async def lesbianrate_error(ctx, error):
 		await ctx.send(f"**{ctx.message.author.name}** is **__{int(random.randint(0, 100))}__**% lesbian! 🏳️‍🌈")
 	elif isinstance(error, commands.BadArgument):
 		embed = discord.Embed(color=0xFFFFFF)
-        embed.add_field(name="Error: Member not found. Mommy was selected as the default member.")
-        await ctx.send(f"**Mommy** is **__not lesbian__**, duh.", embed=embed)
+		embed.add_field(name="Error: Member not found. Mommy was selected as the default member.")
+		await ctx.send(f"**Mommy** is **__not lesbian__**, duh.", embed=embed)
 	else:
 		print('Ignoring exception in command av:', file=sys.stderr)
 		traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 		embed = discord.Embed(description="{}".format(error), color=0x000000)
-        embed.set_footer(text="© MommyBot by Shiki.", icon_url=bot.user.avatar_url)
+		embed.set_footer(text="© MommyBot by Shiki.", icon_url=bot.user.avatar_url)
 		await ctx.send("An error has occured. Detailed information below:", embed=embed)
