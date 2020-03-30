e20101683ee095aba1cf7d05469d7ef53dab0ee3
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-03-30 14:21:32 +0300

    Create howhot.py

---
+++ features/fun/howhot.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import traceback
+import datetime
+import asyncio
+import random
+from datetime import datetime
+from storage import *
+ 
+@bot.command(aliases=["howhot", "ratehot"])
+async def hotrate(ctx, user: discord.Member):
+    await ctx.send(f"**{user.name}** is **__{int(random.randint(0, 100))}__**% hot! 🥵")
+
+@hotrate.error
+async def hotrate_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f"**{ctx.message.author.name}** is **__{int(random.randint(0, 100))}__**% hot! 🥵")
+    elif isinstance(error, commands.BadArgument):
+        embed = discord.Embed(color=0xFFFFFF)
+		embed.add_field(name="Error: Member not found. Mommy was selected as the default member.")
+        await ctx.send(f"**Mommy** is **__too hot to be rated__**, duh.", embed=embed)
+    else:
+        print('Ignoring exception in command av:', file=sys.stderr)
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+        embed = discord.Embed(description="{}".format(error), color=0x000000)
+        embed.set_footer(text="© MommyBot by Shiki.", icon_url=bot.user.avatar_url)
+        await ctx.send("An error has occured. Detailed information below:", embed=embed)
