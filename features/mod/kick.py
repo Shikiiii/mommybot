7d2303ec469319f092e97a20a15d49d923483023
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-04-10 13:30:22 +0300

    Update kick.py

--- features/mod/kick.py
+++ features/mod/kick.py
@@ -30,6 +30,30 @@ async def kick_error(ctx, error):
 		await ctx.send("An error occurupted this command.", embed=embed)
 		return
 	elif isinstance(error, commands.MissingRequiredArgument):
-		
+		user = await commands.MessageConverter().convert(ctx, ctx.message.content[6:])
+		if user is None:
+			embed = discord.Embed(color=0xFFFFFF)
+			embed.add_field(name="Error: Member not found. Please make sure you provide the correct information of a member.", value="Having problems using the command? Contact a staff member!")
+			await ctx.send("An error occurupted this command.", embed=embed)
+			return
+		try:
+			await user.kick(reason="By {}".format(ctx.message.author))
+		except:
+			await ctx.send("Uh! This member has a role higher than mines, I can't kick them.")
+			return
+		try:
+			await user.send("You've been kicked from **{}** by **{}** ({}) for: \n```NO_REASON_PROVIDED```".format(ctx.message.author.guild.name, ctx.message.author, ctx.message.author.id))
+		except:
+			await ctx.send("**{} has been kicked, but they were not notified due to their DMs being closed.** <a:animated_check_emoji:698107059106742372>".format(user.name))
+			return
+		await ctx.send("**{} has been kicked.** <a:animated_check_emoji:698107059106742372>".format(user.name))
 	elif isinstance(error, commands.CheckFailure):
-	
+		embed = discord.Embed(color=0xFFFFFF)
+		embed.add_field(name="Error: You're missing required permissions. You require the ``kick_members`` permission.", value="Having problems using the command? Contact a staff member!")
+		await ctx.send("An error occurupted this command.", embed=embed)
+	else:
+		print('Ignoring exception in command av:', file=sys.stderr)
+		traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+		embed = discord.Embed(description="{}".format(error), color=0x000000)
+		embed.set_footer(text="© MommyBot by Shiki.", icon_url=bot.user.avatar_url)
+		await ctx.send("An error has occured. Detailed information below:", embed=embed)
