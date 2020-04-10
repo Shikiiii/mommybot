7547f3ca51362ea8a745817817e3df63b84e0513
Shikii_ <33571437+Shikiiii@users.noreply.github.com>
2020-04-10 12:25:58 +0300

    Create editsnipe.py

---
+++ features/general/editsnipe.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import traceback
+import datetime
+import asyncio
+import random
+from datetime import datetime
+from storage import *
+
+@bot.command()
+async def editsnipe(ctx):
+	try:
+		to_slice = editsnipe_msgs[str(ctx.message.channel.id)]
+		sniping = to_slice.split("|")
+	except:
+		embed = discord.Embed(color=0xFFFFFF)
+		embed.add_field(name="Error: No snipeable message was found. Was anything edited?", value="Having problems using the command? Contact a staff member!")
+		await ctx.send("An error occurupted this command.", embed=embed)
+	try:
+		author = ctx.guild.get_member(int(sniping[1]))
+	except:
+		embed = discord.Embed(description="{}".format(sniping[0]), color=0xFFFFFF, timestamp=editsnipe_msgs_time[str(ctx.message.channel.id)])
+		embed.set_author(name="User has left the server.".format(author.name), icon_url="https://cdn.discordapp.com/attachments/680647068770893847/698100598930669578/14-147016_question-mark-transparent-png-transparent-background-black-and.png")
+		await ctx.send("**Note:** The member who sent this message has left the server.", embed=embed)
+		return
+	embed = discord.Embed(description="{}".format(sniping[0]), color=0xFFFFFF, timestamp=editsnipe_msgs_time[str(ctx.message.channel.id)])
+	embed.set_author(name="{}".format(author.name), icon_url=author.avatar_url)
+	await ctx.send(embed=embed)
